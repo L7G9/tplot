6c01fa1252fd34dced76a30715dfc8a8a4db2224
l7g9 <lukewgregory@gmail.com>
2023-07-30 00:20:09 +0100

    feat(age_timelines): add age_timeline_edit view

--- tplot_site/age_timelines/views.py
+++ tplot_site/age_timelines/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import AgeTimeline
+
+
+def age_timeline_edit(request, age_timeline_id):
+    age_timeline = get_object_or_404(AgeTimeline, pk=age_timeline_id)
+    return render(
+        request,
+        "age_timelines/age_timeline_edit.html",
+        {"timeline": age_timeline, "age_timeline": age_timeline}
+    )
--- tplot_site/timelines/views.py
+++ tplot_site/timelines/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from age_timelines.models import AgeTimeline
+
+
+def user_timelines(request):
+    age_timeline_list = AgeTimeline.objects.all()
+    return render(
+        request,
+        "timelines/user_timelines.html",
+        {"age_timeline_list": age_timeline_list}
+    )
