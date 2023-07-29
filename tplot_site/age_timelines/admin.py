d989062d7fc2f6a419bed8671f2c39228038f8e6
l7g9 <lukewgregory@gmail.com>
2023-07-29 15:37:57 +0100

    feat(age_timelines): add AgeTimeline and AgeEvent models

--- tplot_site/age_timelines/admin.py
+++ tplot_site/age_timelines/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AgeTimeline, AgeEvent
+
+
+admin.site.register(AgeTimeline)
+admin.site.register(AgeEvent)
--- tplot_site/age_timelines/models.py
+++ tplot_site/age_timelines/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from timelines import models as timelines
 
-# Create your models here.
+
+class AgeTimeline(timelines.Timeline):
+    SCALE_UNITS = [
+        (1, "1 Year"),
+        (5, "5 Years"),
+        (10, "10 Years"),
+        (20, "20 Years"),
+        (25, "25 Years"),
+        (50, "50 Years"),
+        (100, "100 Years"),
+    ]
+    scale_unit = models.PositiveSmallIntegerField(choices=SCALE_UNITS, default=3)
+
+
+class AgeEvent(timelines.Event):
+    age_timeline = models.ForeignKey(AgeTimeline, on_delete=models.CASCADE)
+    start_year = models.SmallIntegerField(default=0)
+    start_month = models.SmallIntegerField(default=0)
+    end_year = models.SmallIntegerField(default=0)
+    end_month = models.SmallIntegerField(default=0)
