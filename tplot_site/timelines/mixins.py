2738c55a7995acfb4dded4488569375e41c80d62
l7g9 <lukewgregory@gmail.com>
2023-08-04 00:03:37 +0100

    feat(OwnerRequiredMixin): create mixin to check object loaded by from pk in kwargs belongs to logged in user

---
+++ tplot_site/timelines/mixins.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponseForbidden
+
+
+class OwnerRequiredMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().get_owner() != self.request.user:
+            return HttpResponseForbidden()
+        return super(OwnerRequiredMixin, self).dispatch(request, *args, **kwargs)
