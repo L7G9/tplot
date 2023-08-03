db149c5f95629d670015314e1fd899a41536dd82
l7g9 <lukewgregory@gmail.com>
2023-08-04 00:01:37 +0100

    feat(user_timelines): filter by user

--- tplot_site/timelines/views.py
+++ tplot_site/timelines/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 
 from age_timelines.models import AgeTimeline
 
 
+@login_required(login_url="/accounts/login/")
 def user_timelines(request):
-    age_timeline_list = AgeTimeline.objects.all()
+    age_timeline_list = AgeTimeline.objects.all().filter(user=request.user)
     return render(
         request,
         "timelines/user_timelines.html",
