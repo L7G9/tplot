ad51a3a4cd275cfa8608d48786e873121ce46300
l7g9 <lukewgregory@gmail.com>
2023-08-10 23:08:19 +0100

    refactor(setUpTestData): remove unused variable

--- tplot_site/timelines/tests/test_views.py
+++ tplot_site/timelines/tests/test_views.py
@@ -9,8 +9,6 @@ class UserTimelinesViewTest(TestCase):
 
     @classmethod
     def setUpTestData(self):
-        age_timeline_count = 3
-
         test_user_1 = User.objects.create_user(
             username="TestUser1",
             password="TestUser1#"
@@ -36,26 +34,26 @@ class UserTimelinesViewTest(TestCase):
         self.assertRedirects(response, "/accounts/login/?next=/timelines/")
 
     def test_view_url_exists_at_desired_location(self):
-        login = self.client.login(username="TestUser1", password="TestUser1#")
+        self.client.login(username="TestUser1", password="TestUser1#")
         response = self.client.get("/timelines/")
         self.assertEqual(str(response.context['user']), "TestUser1")
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        login = self.client.login(username="TestUser1", password="TestUser1#")
+        self.client.login(username="TestUser1", password="TestUser1#")
         response = self.client.get(reverse("timelines:user-timelines"))
         self.assertEqual(str(response.context['user']), "TestUser1")
         self.assertEqual(response.status_code, 200)
 
     def test_uses_correct_template(self):
-        login = self.client.login(username="TestUser1", password="TestUser1#")
+        self.client.login(username="TestUser1", password="TestUser1#")
         response = self.client.get(reverse("timelines:user-timelines"))
         self.assertEqual(str(response.context['user']), "TestUser1")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "timelines/user_timelines.html")
 
     def test_lists_age_timelines_owned_by_logged_in_user(self):
-        login = self.client.login(username="TestUser1", password="TestUser1#")
+        self.client.login(username="TestUser1", password="TestUser1#")
         response = self.client.get(reverse("timelines:user-timelines"))
         self.assertEqual(str(response.context['user']), "TestUser1")
         self.assertEqual(response.status_code, 200)
@@ -63,4 +61,7 @@ class UserTimelinesViewTest(TestCase):
         age_timeline_list = response.context['age_timeline_list']
         self.assertEqual(len(age_timeline_list), self.TIMELINE_COUNT)
         for age_timeline in age_timeline_list:
-            self.assertEqual(age_timeline.get_owner(), response.context['user'])
+            self.assertEqual(
+                age_timeline.get_owner(),
+                response.context['user']
+            )
