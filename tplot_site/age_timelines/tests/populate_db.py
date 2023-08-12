c611040dda4d9a533b819cf2ccce671f669791ea
l7g9 <lukewgregory@gmail.com>
2023-08-12 23:24:50 +0100

    test(populate_db): add return data structure

--- tplot_site/age_timelines/tests/populate_db.py
+++ tplot_site/age_timelines/tests/populate_db.py
@@ -10,16 +10,51 @@ def populate_db(
         tags_per_timeline,
         areas_per_timeline
 ):
+    users = []
     for user_index in range(user_count):
         user = User.objects.create_user(
-            username=f"test_user_{user_index}",
-            password=f"TestUser{user_index}#"
+            username=f"User{user_index}",
+            password=f"Password{user_index}#"
         )
+        user_ids = {
+            'username': f"User{user_index}",
+            'password': f"Password{user_index}#",
+            'age_timeline_ids': []
+        }
+        users.append(user_ids)
         for age_timeline_index in range(age_timelines_per_user):
-            age_timeline = AgeTimeline.objects.create(user=user, title=f"{user.username} AgeTimeline {age_timeline_index}")
+            age_timeline = AgeTimeline.objects.create(
+                user=user,
+                title=f"AgeTimeline{age_timeline_index}"
+            )
+            age_timeline_ids = {
+                'id': age_timeline.id,
+                'age_event_ids': [],
+                'tag_ids': [],
+                'event_area_ids': [],
+            }
+            user_ids['age_timeline_ids'].append(age_timeline_ids)
+
             for age_event_index in range(events_per_timeline):
-                AgeEvent.objects.create(age_timeline=age_timeline, timeline_id=age_timeline.timeline_ptr.pk, title=f"event {age_event_index}")
+                age_event = AgeEvent.objects.create(
+                    age_timeline=age_timeline,
+                    timeline_id=age_timeline.timeline_ptr.pk,
+                    title=f"Event{age_event_index}"
+                )
+                age_timeline_ids['age_event_ids'].append(age_event.id)
+
             for tag_index in range(tags_per_timeline):
-                Tag.objects.create(timeline=age_timeline.timeline_ptr, name=f"tag {tag_index}")
+                tag = Tag.objects.create(
+                    timeline=age_timeline.timeline_ptr,
+                    name=f"Tag{tag_index}"
+                )
+                age_timeline_ids['tag_ids'].append(tag.id)
+
             for area_index in range(areas_per_timeline):
-                EventArea.objects.create(timeline=age_timeline.timeline_ptr, name=f"area {area_index}", page_position=area_index+1)
+                event_area = EventArea.objects.create(
+                    timeline=age_timeline.timeline_ptr,
+                    name=f"Area{area_index}", page_position=area_index+1
+                )
+                age_timeline_ids['event_area_ids'].append(event_area.id)
+
+    return users
