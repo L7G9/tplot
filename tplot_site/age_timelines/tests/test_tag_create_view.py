c64e876f09ee0b4bf37827bbbbe319106c837016
l7g9 <lukewgregory@gmail.com>
2023-08-14 21:55:58 +0100

    tests(TagCreateViewTest): add units tests for tagCreateView

---
+++ tplot_site/age_timelines/tests/test_tag_create_view.py
@@ -0,0 +1,142 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from .populate_db import populate_db
+from timelines.models import Tag, Timeline
+
+
+class TagCreateViewTest(TestCase):
+    USER_COUNT = 2
+    AGE_TIMELINES_PER_USER = 1
+
+    @classmethod
+    def setUpTestData(cls):
+        users = populate_db(
+            cls.USER_COUNT,
+            cls.AGE_TIMELINES_PER_USER,
+            0,
+            0,
+            0
+        )
+        cls.user0 = User.objects.get(username=users[0]['username'])
+        cls.user0_password = users[0]['password']
+        cls.user0_age_timeline_id = users[0]['age_timeline_ids'][0]['id']
+        age_timeline0 = Timeline.objects.get(user=cls.user0)
+        cls.user0_timeline_id = age_timeline0.pk
+        cls.user1_age_timeline_id = users[1]['age_timeline_ids'][0]['id']
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            )
+        )
+        self.assertRedirects(
+            response,
+            f"/accounts/login/?next=/timelines/age/{self.user0_age_timeline_id}/tag/add/"
+        )
+
+    def test_forbidden_if_age_timeline_not_owned_by_logged_in_user(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        response = self.client.get(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user1_age_timeline_id}
+            )
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        response = self.client.get(
+            f"/timelines/age/{self.user0_age_timeline_id}/tag/add/"
+        )
+        self.assertEqual(str(response.context['user']), self.user0.username)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        response = self.client.get(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            )
+        )
+        self.assertEqual(str(response.context['user']), self.user0.username)
+        self.assertEqual(response.status_code, 200)
+
+    def test_uses_correct_template(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        response = self.client.get(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            )
+        )
+        self.assertTemplateUsed(
+            response,
+            "age_timelines/tag_add_form.html"
+        )
+
+    def test_tag_added(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        data = {
+            'timeline': self.user0_timeline_id,
+            'name': 'New Tag',
+        }
+        response = self.client.post(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            ),
+            data=data,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        age_events = Tag.objects.filter(
+            timeline=self.user0_timeline_id
+        )
+        self.assertEquals(len(age_events), 1)
+
+    def test_redirect_after_tag_added(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        data = {
+            'timeline': self.user0_timeline_id,
+            'name': 'New Tag',
+        }
+        response = self.client.post(
+            reverse(
+                "age_timelines:tag-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            ),
+            data=data,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            reverse(
+                "age_timelines:age-timeline-detail",
+                kwargs={'pk': self.user0_age_timeline_id}
+            ),
+            status_code=302
+        )
