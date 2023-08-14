3cff4380a15385790c3490431ea65a03cce418e0
l7g9 <lukewgregory@gmail.com>
2023-08-14 22:33:08 +0100

    style(AgeEventCreateViewTest): created attribute new_age_event_data and calculated expected_age_events

--- tplot_site/age_timelines/tests/test_age_event_create_view.py
+++ tplot_site/age_timelines/tests/test_age_event_create_view.py
@@ -8,18 +8,32 @@ from age_timelines.models import AgeEvent
 class AgeEventCreateViewTest(TestCase):
     USER_COUNT = 2
     AGE_TIMELINES_PER_USER = 1
+    EVENTS_PER_TIMELINE = 1
 
     @classmethod
     def setUpTestData(cls):
         users = populate_db(
             cls.USER_COUNT,
             cls.AGE_TIMELINES_PER_USER,
-            0, 0, 0
+            cls.EVENTS_PER_TIMELINE,
+            0,
+            0
         )
         cls.user0 = User.objects.get(username=users[0]['username'])
         cls.user0_password = users[0]['password']
         cls.user0_age_timeline_id = users[0]['age_timeline_ids'][0]['id']
         cls.user1_age_timeline_id = users[1]['age_timeline_ids'][0]['id']
+
+        cls.new_age_event_data = {
+            'age_timeline': cls.user0_age_timeline_id,
+            'title': 'New Age Event',
+            'description': 'Description',
+            'start_year': 1,
+            'start_month': 0,
+            'has_end': False,
+            'end_year': 2,
+            'end_month': 0
+        }
 
     def test_redirect_if_not_logged_in(self):
         response = self.client.get(
@@ -33,7 +47,7 @@ class AgeEventCreateViewTest(TestCase):
             f"/accounts/login/?next=/timelines/age/{self.user0_age_timeline_id}/event/add/"
         )
 
-    def test_forbidden_if_not_owned_by_logged_in_user(self):
+    def test_forbidden_if_age_timeline_not_owned_by_logged_in_user(self):
         self.client.login(
             username=self.user0.username,
             password=self.user0_password
@@ -87,34 +101,40 @@ class AgeEventCreateViewTest(TestCase):
             "age_timelines/age_event_add_form.html"
         )
 
-    def test_age_event_added_and_redirect(self):
+    def test_age_event_added(self):
         self.client.login(
             username=self.user0.username,
             password=self.user0_password
         )
-        data = {
-            'age_timeline': self.user0_age_timeline_id,
-            'title': 'New Age Event',
-            'description': 'Description',
-            'start_year': 1,
-            'start_month': 0,
-            'has_end': False,
-            'end_year': 2,
-            'end_month': 0
-        }
         response = self.client.post(
             reverse(
                 "age_timelines:age-event-add",
                 kwargs={'age_timeline_id': self.user0_age_timeline_id}
             ),
-            data=data,
+            data=self.new_age_event_data,
             follow=True
         )
         self.assertEquals(response.status_code, 200)
         age_events = AgeEvent.objects.filter(
             age_timeline=self.user0_age_timeline_id
         )
-        self.assertEquals(len(age_events), 1)
+        expected_age_events = (self.AGE_TIMELINES_PER_USER * self.EVENTS_PER_TIMELINE) + 1
+        self.assertEquals(len(age_events), expected_age_events)
+
+    def test_redirect_after_age_event_added(self):
+        self.client.login(
+            username=self.user0.username,
+            password=self.user0_password
+        )
+        response = self.client.post(
+            reverse(
+                "age_timelines:age-event-add",
+                kwargs={'age_timeline_id': self.user0_age_timeline_id}
+            ),
+            data=self.new_age_event_data,
+            follow=True
+        )
+        self.assertEquals(response.status_code, 200)
         self.assertRedirects(
             response,
             reverse(
