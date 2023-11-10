1281b92018e33299d2629806fba0a614f15e9d06
l7g9 <lukewgregory@gmail.com>
2023-11-10 13:17:29 +0000

    refactor(TimelineLayout): update all measurments from mm to canvas units
    prevents multiple unit types and the need to convert to and from each type of unit

--- timelines/pdf/timeline_layout.py
+++ timelines/pdf/timeline_layout.py
@@ -1,18 +1,16 @@
+from reportlab.lib.units import mm
 from timelines.models import Timeline
 from .area import Area
-from .scale_description import ScaleDescription
 
 
-A3_LONG = 420
-A3_SHORT = 297
-A4_LONG = 297
-A4_SHORT = 210
-A5_LONG = 210
-A5_SHORT = 148
+A3_LONG = 420 * mm
+A3_SHORT = 297 * mm
+A4_LONG = 297 * mm
+A4_SHORT = 210 * mm
+A5_LONG = 210 * mm
+A5_SHORT = 148 * mm
 
-DEFAULT_BORDER_SIZE = 10
-DEFAULT_SCALE_SIZE = 6
-DEFAULT_SCALE_UNIT_LINE_LENGTH = 3
+DEFAULT_BORDER_SIZE = 10 * mm
 
 
 class TimelineLayout:
@@ -26,79 +24,110 @@ class TimelineLayout:
     def __init__(
         self,
         timeline: Timeline,
-        scale_description: ScaleDescription,
         border_size=DEFAULT_BORDER_SIZE,
-        scale_size=DEFAULT_SCALE_SIZE,
-        scale_unit_line_length=DEFAULT_SCALE_UNIT_LINE_LENGTH,
     ):
         self.timeline = timeline
-        self.scale_description = scale_description
         self.border_size = border_size
-        self.scale_size = scale_size
-        self.scale_unit_line_length = scale_unit_line_length
 
-        # TODO - use something better then "L"
-        if self.timeline.page_orientation == "L":
-            self.page_area = self.__landscape_page_area()
-        else:
-            self.page_area = self.__portrait_page_area()
-
-        self.drawable_area = self.__drawable_area(self.page_area)
-
+        self.page_area = self.__initial_page_area()
+        self.drawable_area: Area = self.__drawable_area(self.page_area)
         self.title_area: Area = None
         self.description_area: Area = None
         self.event_and_scale_area: Area = None
         self.scale_area: Area = None
         self.event_areas: [Area] = []
 
-    def create_layout(self, title_height, description_height):
+    def create_layout(
+        self, title_height, description_height, scale_width, scale_height
+    ):
         """Calculate areas for all the elements of the timeline to be
-        displayed in mm.
+        displayed.
         """
 
         if self.timeline.page_orientation == "L":
-            self.__landscape_layout(title_height, description_height)
+            self.__landscape_layout(
+                title_height, description_height, scale_width, scale_height
+            )
         else:
-            self.__portrait_layout(title_height, description_height)
+            self.__portrait_layout(
+                title_height, description_height, scale_width, scale_height
+            )
 
-    def __landscape_layout(self, title_height, description_height):
-        """Calculate areas for all the elements of the timeline to be
-        displayed in mm.
+    def __landscape_layout(
+        self, title_height, description_height, scale_width, scale_height
+    ):
+        """Calculate areas for all the elements of the landscape timeline to
+        be displayed.
         """
+        self.page_area = self.__landscape_page_area(scale_width)
+        self.drawable_area = self.__drawable_area(self.page_area)
+
         self.title_area = self.__title_area(title_height)
         self.description_area = self.__description_area(description_height)
         self.event_and_scale_area = self.__event_and_scale_area()
 
         total_weight = self.__total_event_area_weight()
-
         height_per_weight = self.__size_per_weight(
-            self.event_and_scale_area.height, self.scale_size, total_weight
+            self.event_and_scale_area.height, scale_height, total_weight
         )
-        self.__landscape_event_and_scale_layout(height_per_weight)
+        self.__landscape_event_and_scale_layout(
+            height_per_weight, scale_width, scale_height
+        )
 
-    def __portrait_layout(self, title_height, description_height):
-        """Calculate areas for all the elements of the timeline to be
-        displayed in mm.
+    def __portrait_layout(
+        self, title_height, description_height, scale_width, scale_height
+    ):
+        """Calculate areas for all the elements of the portrait timeline to be
+        displayed.
         """
-        height_increase = title_height + description_height + self.border_size
-        self.page_area.height += height_increase
-        self.drawable_area.height += height_increase
+
+        self.page_area = self.__portrait_page_area(
+            title_height, description_height, scale_height
+        )
+        self.drawable_area = self.__drawable_area(self.page_area)
 
         self.title_area = self.__title_area(title_height)
         self.description_area = self.__description_area(description_height)
-        self.event_and_scale_area = self.__event_and_scale_area(
-            description_border=self.border_size
-        )
+        self.event_and_scale_area = self.__event_and_scale_area()
 
         total_weight = self.__total_event_area_weight()
         width_per_weight = self.__size_per_weight(
-            self.event_and_scale_area.width, self.scale_size, total_weight
+            self.event_and_scale_area.width, scale_width, total_weight
         )
-        self.__portrait_event_and_scale_layout(width_per_weight)
+        self.__portrait_event_and_scale_layout(
+            width_per_weight, scale_width, scale_height
+        )
 
-    def __landscape_page_area(self) -> Area:
-        """Calculate area of the whole PDF in mm."""
-        width = self.scale_description.scale_length + (2 * self.border_size)
+    def __initial_page_area(self) -> Area:
+        """Calculate an initial area of the PDF which can be used to measure
+        text on."""
+        # TODO - use something better then "3" & "L"
+        if self.timeline.page_orientation == "L":
+            if self.timeline.page_size == "3":
+                width = A3_LONG
+                height = A3_SHORT
+            elif self.timeline.page_size == "4":
+                width = A4_LONG
+                height = A4_SHORT
+            else:
+                width = A5_LONG
+                height = A5_SHORT
+        else:
+            if self.timeline.page_size == "3":
+                width = A3_SHORT
+                height = A3_LONG
+            elif self.timeline.page_size == "4":
+                width = A4_SHORT
+                height = A4_LONG
+            else:
+                width = A5_SHORT
+                height = A5_LONG
+
+        return Area(0, 0, width, height)
+
+    def __landscape_page_area(self, scale_width):
+        """Calculate area of the whole PDF."""
+        width = scale_width + (2 * self.border_size)
         # TODO - use something better then "3"
         if self.timeline.page_size == "3":
             height = A3_SHORT
@@ -109,9 +138,16 @@ class TimelineLayout:
 
         return Area(0, 0, width, height)
 
-    def __portrait_page_area(self) -> Area:
-        """Calculate area of the whole PDF in mm."""
-        height = self.scale_description.scale_length + (2 * self.border_size)
+    def __portrait_page_area(
+        self, title_height, description_height, scale_height
+    ) -> Area:
+        """Calculate area of the whole PDF."""
+        height = (
+            title_height
+            + description_height
+            + scale_height
+            + (2 * self.border_size)
+        )
         # TODO - use something better then "3"
         if self.timeline.page_size == "3":
             width = A3_SHORT
@@ -122,8 +158,8 @@ class TimelineLayout:
 
         return Area(0, 0, width, height)
 
-    def __drawable_area(self, page_area) -> Area:
-        """Calculate drawable area a border's width inside page_area in mm."""
+    def __drawable_area(self, page_area: Area) -> Area:
+        """Calculate drawable area a border's width inside page_area."""
         return Area(
             self.border_size,
             self.border_size,
@@ -150,17 +186,19 @@ class TimelineLayout:
             description_height,
         )
 
-    def __event_and_scale_area(self, description_border=0) -> Area:
+    def __event_and_scale_area(self) -> Area:
         """Calculate combined area for timeline's events and scale between the
         description and the bottom of the drawable area."""
         return Area(
             self.drawable_area.x,
             self.drawable_area.y,
             self.drawable_area.width,
-            self.description_area.y - self.drawable_area.y - description_border,
+            self.description_area.y - self.drawable_area.y,
         )
 
-    def __landscape_event_and_scale_layout(self, height_per_weight):
+    def __landscape_event_and_scale_layout(
+        self, height_per_weight, scale_width, scale_height
+    ):
         """Calculate areas for each event area and scale in a landscape
         timeline."""
         scale_position_calculated = False
@@ -172,8 +210,10 @@ class TimelineLayout:
                 <= timeline_event_area.page_position
             )
             if (scale_position_calculated is False) and scale_position_reached:
-                self.scale_area = self.__landscape_scale_area(next_y)
-                next_y += self.scale_size
+                self.scale_area = Area(
+                    self.drawable_area.x, next_y, scale_width, scale_height
+                )
+                next_y += scale_height
                 scale_position_calculated = True
 
             event_area_height = timeline_event_area.weight * height_per_weight
@@ -185,9 +225,13 @@ class TimelineLayout:
             next_y += event_area_height
 
         if scale_position_calculated is False:
-            self.scale_area = self.__landscape_scale_area(next_y)
+            self.scale_area = Area(
+                self.drawable_area.x, next_y, scale_width, scale_height
+            )
 
-    def __portrait_event_and_scale_layout(self, width_per_weight):
+    def __portrait_event_and_scale_layout(
+        self, width_per_weight, scale_width, scale_height
+    ):
         """Calculate areas for each event area and scale in a portrait
         timeline."""
         scale_position_calculated = False
@@ -199,8 +243,10 @@ class TimelineLayout:
                 <= timeline_event_area.page_position
             )
             if (scale_position_calculated is False) and scale_position_reached:
-                self.scale_area = self.__portrait_scale_area(next_x)
-                next_x += self.scale_size
+                self.scale_area = Area(
+                    next_x, self.drawable_area.y, scale_width, scale_height
+                )
+                next_x += scale_width
                 scale_position_calculated = True
 
             event_area_width = timeline_event_area.weight * width_per_weight
@@ -212,25 +258,9 @@ class TimelineLayout:
             next_x += event_area_width
 
         if scale_position_calculated is False:
-            self.scale_area = self.__portrait_scale_area(next_x)
-
-    def __landscape_scale_area(self, y) -> Area:
-        """Calculate area for the scale in a landscape timeline."""
-        return Area(
-            0,
-            y,
-            self.page_area.width,
-            self.scale_size,
-        )
-
-    def __portrait_scale_area(self, x) -> Area:
-        """Calculate area for the scale in a portrait timeline."""
-        return Area(
-            x,
-            0,
-            self.scale_size,
-            self.event_and_scale_area.height + (2 * self.border_size),
-        )
+            self.scale_area = Area(
+                next_x, self.drawable_area.y, scale_width, scale_height
+            )
 
     def __landscape_event_area(self, y, height, event_area) -> Area:
         """Calculate area for an event area in a landscape timeline."""
