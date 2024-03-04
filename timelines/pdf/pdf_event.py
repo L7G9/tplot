940eff52e9ab538a98c7dc34161d45a9a33eb9df
l7g9 <lukewgregory@gmail.com>
2024-03-04 14:37:16 +0000

    refactor: reworked classes to draw timeline's events

--- timelines/pdf/pdf_event.py
+++ timelines/pdf/pdf_event.py
@@ -1,13 +1,43 @@
+"""Contains class to draw an event with a start time and no end time on a
+Canvas.
+
+Classes:
+    PDFEvent
+"""
+
 from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.units import mm
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Paragraph
+
 from .area import Area
+
+"""Ideal ratio between width and height of a PDFEvent, if possible should be
+twice as wide a high."""
+WIDTH_HEIGHT_RATIO = 2.0
+
+"""Fraction to reduce width of PDFEvent by until it meets constraints of
+width / height ratio, minimum width and maximum height """
+INCREMENT_SIZE = 0.1
 
 
 class PDFEvent(Area):
-    """Class to draw an Timeline Event with a start time on a Canvas."""
+    """Class to draw an Timeline Event with a start time on a Canvas.
+
+    Extends:
+        Area
+
+    Attributes:
+        time_paragraph: A Paragraph displaying the time when the Event
+        occurred.
+        title_paragraph: A Paragraph displaying the title of the Event.
+        description_paragraph: A Paragraph displaying the description of the
+        Event.
+        border_size: A float storing the gap between the Paragraphs and their
+        bounding box.
+        canvas: A Canvas to draw the Event on.
+    """
+
     def __init__(
         self,
         time: str,
@@ -18,74 +48,94 @@ class PDFEvent(Area):
         time_style: ParagraphStyle,
         title_style: ParagraphStyle,
         description_style: ParagraphStyle,
+        border_size: float,
         max_width: float = 0,
         max_height: float = 0,
     ):
+        """Initialise Instance.
+
+        Measures width how long each paragraph will be then creates a
+        PDFEvent instance of a suitable size and layout to fit into the
+        max_width or max_height depending on the timeline orientation.
+
+        Args:
+            time: A String describing when the event occurred.
+            title: A String holding the event's title.
+            description: A String describing the event in detail.
+            orientation: A String holding if the event is to be drawn is from
+            a landscape or portrait timeline.
+            canvas: A Canvas to draw the PDFEvent on.
+            time_style: A ParagraphStyle for the time.
+            title_paragraph: A ParagraphStyle for the title.
+            description_style: A ParagraphStyle for the description.
+            border_size: A float storing the gap between the Paragraphs and
+            their bounding box.
+            max_width: A float storing the maximum width the PDFEvent can be
+            on a portrait timeline.
+            max_height: A float storing the maximum width the PDFEvent can be
+            on a landscape timeline.
+
+        Raises:
+            ValueError: If orientation is not L for landscape or P for
+            portrait.
+        """
         if (orientation != "L") and (orientation != "P"):
-            raise ValueError("orientation must be L for landscape or P for portrait")
+            raise ValueError(
+                "orientation must be L for landscape or P for portrait"
+            )
 
         self.time_paragraph: Paragraph = Paragraph(time, time_style)
         self.title_paragraph: Paragraph = Paragraph(title, title_style)
-        self.description_paragraph: Paragraph = Paragraph(description, description_style)
+        self.description_paragraph: Paragraph = Paragraph(
+            description, description_style
+        )
+        self.border_size = border_size
         self.canvas = canvas
 
         self.x = 0
         self.y = 0
 
         time_width = stringWidth(
-            time,
-            time_style.fontName,
-            time_style.fontSize
+            time, time_style.fontName, time_style.fontSize
         )
         title_width = stringWidth(
-            title,
-            title_style.fontName,
-            title_style.fontSize
+            title, title_style.fontName, title_style.fontSize
         )
         description_width = stringWidth(
-            description,
-            description_style.fontName,
-            description_style.fontSize
+            description, description_style.fontName, description_style.fontSize
         )
-
 
         if orientation == "L":
             self.width, self.height = self.__landscape_init(
                 time_width,
                 title_width,
                 description_width,
-                2,
-                max_height
+                WIDTH_HEIGHT_RATIO,
+                max_height,
             )
         elif orientation == "P":
             self.width, self.height = self.__portrait_init(
                 time_width,
                 title_width,
                 description_width,
-                2,
-                max_width
+                WIDTH_HEIGHT_RATIO,
+                max_width,
             )
 
-        self.time_paragraph.wrapOn(
-            self.canvas,
-            self.width,
-            0
-        )
-        self.title_paragraph.wrapOn(
-            self.canvas,
-            self.width,
-            0
-        )
-        self.description_paragraph.wrapOn(
-            self.canvas,
-            self.width,
-            0
-        )
+        max_text_width = self.width - (2 * self.border_size)
+        self.time_paragraph.wrapOn(self.canvas, max_text_width, 0)
+        self.title_paragraph.wrapOn(self.canvas, max_text_width, 0)
+        self.description_paragraph.wrapOn(self.canvas, max_text_width, 0)
 
-    def __get_dimensions(self, width):
+    def _get_dimensions(self, width: float):
+        """Calculates the total width and height needed to display the time,
+        title and description Paragraphs for a given width."""
         time_width, time_height = self.time_paragraph.wrap(width, 0)
         title_width, title_height = self.title_paragraph.wrap(width, 0)
-        description_width, description_height = self.description_paragraph.wrap(width, 0)
+        (
+            description_width,
+            description_height,
+        ) = self.description_paragraph.wrap(width, 0)
         max_width = max(max(time_width, title_width), description_width)
         total_height = time_height + title_height + description_height
 
@@ -93,62 +143,81 @@ class PDFEvent(Area):
 
     def __landscape_init(
         self,
-        time_width,
-        title_width,
-        description_width,
+        time_width: float,
+        title_width: float,
+        description_width: float,
         target_ratio: float,
-        max_height: float
+        max_height: float,
     ):
+        """Calculates the best width and height for a landscape PDFEvent given
+        the constraints of a target ratio for width/height and the maximum
+        height available to draw it in."""
         init_width = max(max(time_width, title_width), description_width)
         min_width = time_width
 
-        width, height = self.__get_dimensions(init_width)
-        best_width, best_hight = width, height
-        width_reduction_increment = width * 0.1
+        width, height = self._get_dimensions(init_width)
+        best_width, best_height = width, height
+        width_reduction_increment = width * INCREMENT_SIZE
 
-        while ((width / height) >= target_ratio) and (width >= min_width) and (height <= max_height):
-            best_width, best_hight = width, height
+        # reduce width until within parameters
+        while (
+            ((width / height) >= target_ratio)
+            and (width >= min_width)
+            and (height <= max_height)
+        ):
+            best_width, best_height = width, height
             new_width = width - width_reduction_increment
-            width, height = self.__get_dimensions(new_width)
+            width, height = self._get_dimensions(new_width)
 
-        return best_width, best_hight
+        return best_width + (2 * self.border_size), best_height
 
     def __portrait_init(
         self,
-        time_width,
-        title_width,
-        description_width,
+        time_width: float,
+        title_width: float,
+        description_width: float,
         target_ratio: float,
-        max_width: float
+        max_width: float,
     ):
+        """Calculates the best width and height for a portrait PDFEvent given
+        the constraints of a target ratio for width/height and the maximum
+        width available to draw it in."""
         largest_width = max(max(time_width, title_width), description_width)
         init_width = min(largest_width, max_width)
         min_width = min(time_width, max_width)
 
-        width, height = self.__get_dimensions(init_width)
-        best_width, best_hight = width, height
-        width_reduction_increment = width * 0.1
+        width, height = self._get_dimensions(init_width)
+        best_width, best_height = width, height
+        width_reduction_increment = width * INCREMENT_SIZE
 
+        # reduce width until within parameters
         while ((width / height) >= target_ratio) and (width >= min_width):
-            best_width, best_hight = width, height
+            best_width, best_height = width, height
             new_width = width - width_reduction_increment
-            width, height = self.__get_dimensions(new_width)
+            width, height = self._get_dimensions(new_width)
 
-        return best_width, best_hight
+        return best_width + (2 * self.border_size), best_height
 
     def draw(self):
+        """Draw PDFEvent on it's canvas."""
         self.time_paragraph.drawOn(
             self.canvas,
-            self.x,
-            self.y + self.title_paragraph.height + self.description_paragraph.height
+            self.x + self.border_size,
+            self.y
+            + self.title_paragraph.height
+            + self.description_paragraph.height,
         )
         self.title_paragraph.drawOn(
             self.canvas,
-            self.x,
-            self.y + self.description_paragraph.height
+            self.x + self.border_size,
+            self.y + self.description_paragraph.height,
         )
         self.description_paragraph.drawOn(
-            self.canvas,
+            self.canvas, self.x + self.border_size, self.y
+        )
+        self.canvas.rect(
             self.x,
-            self.y
+            self.y,
+            self.width,
+            self.height,
         )
--- timelines/pdf/pdf_start_end_event.py
+++ timelines/pdf/pdf_start_end_event.py
@@ -1,13 +1,34 @@
+"""Contains class to draw an event with a start and end time on a Canvas.
+
+Classes:
+    PDFStartEndEvent
+"""
 from reportlab.lib.styles import ParagraphStyle
-from reportlab.lib.units import mm
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Paragraph
-from .area import Area
+
+from .pdf_event import PDFEvent
 
 
-class PDFStartEndEvent(Area):
-    """Class to draw an Timeline Event with a start and end time on a Canvas."""
+class PDFStartEndEvent(PDFEvent):
+    """Class to draw an Timeline Event with a start and end time on a Canvas.
+
+    Extends:
+        PDFEvent
+
+    Attributes:
+        time_paragraph: A Paragraph displaying the time when the Event
+        occurred.
+        title_paragraph: A Paragraph displaying the title of the Event.
+        description_paragraph: A Paragraph displaying the description of the
+        Event.
+        border_size: A float storing the gap between the Paragraphs and their
+        bounding box.
+        canvas: A Canvas to draw the Event on.
+    """
+
+    # TODO: remove duplication with PDFEvent
     def __init__(
         self,
         time: str,
@@ -18,90 +39,100 @@ class PDFStartEndEvent(Area):
         time_style: ParagraphStyle,
         title_style: ParagraphStyle,
         description_style: ParagraphStyle,
+        border_size: float,
         fixed_size: float,
         max_width: float = 0,
-        max_height: float = 0
+        max_height: float = 0,
     ):
+        """Initialise Instance.
 
+        Measures width how long each paragraph will be then creates a
+        PDFEvent instance of a suitable size and layout to fit into the
+        max_width or max_height depending on the timeline orientation.
+
+        Args:
+            time: A String describing when the event occurred.
+            title: A String holding the event's title.
+            description: A String describing the event in detail.
+            orientation: A String holding if the event is to be drawn is from
+            a landscape or portrait timeline.
+            canvas: A Canvas to draw the PDFEvent on.
+            time_style: A ParagraphStyle for the time.
+            title_paragraph: A ParagraphStyle for the title.
+            description_style: A ParagraphStyle for the description.
+            border_size: A float storing the gap between the Paragraphs and
+            their bounding box.
+            fixed_size: A float storing the width or height (depending on
+            orientation) that this event should be on the timeline.
+            max_width: A float storing the maximum width the PDFEvent can be
+            on a portrait timeline.
+            max_height: A float storing the maximum width the PDFEvent can be
+            on a landscape timeline.
+
+        Raises:
+            ValueError: If orientation is not L for landscape or P for
+            portrait.
+        """
         if (orientation != "L") and (orientation != "P"):
-            raise ValueError("orientation must be L for landscape or P for portrait")
+            raise ValueError(
+                "orientation must be L for landscape or P for portrait"
+            )
 
         self.time_paragraph: Paragraph = Paragraph(time, time_style)
         self.title_paragraph: Paragraph = Paragraph(title, title_style)
-        self.description_paragraph: Paragraph = Paragraph(description, description_style)
+        self.description_paragraph: Paragraph = Paragraph(
+            description, description_style
+        )
+        self.border_size = border_size
         self.canvas = canvas
 
         self.x = 0
         self.y = 0
 
         time_width = stringWidth(
-            time,
-            time_style.fontName,
-            time_style.fontSize
+            time, time_style.fontName, time_style.fontSize
         )
         title_width = stringWidth(
-            title,
-            title_style.fontName,
-            title_style.fontSize
+            title, title_style.fontName, title_style.fontSize
         )
         description_width = stringWidth(
-            description,
-            description_style.fontName,
-            description_style.fontSize
+            description, description_style.fontName, description_style.fontSize
         )
 
         if orientation == "L":
-            _, self.height = self.__landscape_init(fixed_size)
+            _, self.height = self.__landscape_init(
+                fixed_size - (2 * self.border_size)
+            )
             self.width = fixed_size
         elif orientation == "P":
-
-            largest_width = max(max(time_width, title_width), description_width)
+            max_width -= 2 * self.border_size
+            largest_width = max(
+                max(time_width, title_width), description_width
+            )
             init_width = min(largest_width, max_width)
             min_width = min(time_width, max_width)
 
-            self.width, _ = self.__portrait_init(fixed_size, min_width, init_width)
+            self.width, _ = self.__portrait_init(
+                fixed_size, min_width, init_width
+            )
             self.height = fixed_size
 
     def __landscape_init(self, width):
-        return self.__get_dimensions(width)
+        """Calculates height for a landscape PDFStartEndEvent given that it
+        needs to be a set width."""
+        return PDFEvent._get_dimensions(self, width)
 
     def __portrait_init(self, height, desired_width, max_width):
+        """Calculates width for a portrait PDFStartEndEvent given that it
+        needs to be a set height."""
         current_width, current_height = self.__get_dimensions(desired_width)
 
         width_increment = (max_width - desired_width) / 5
 
         while current_height > height:
             expanded_width = current_width + width_increment
-            current_width, current_height = self.__get_dimensions(expanded_width)
+            current_width, current_height = self.__get_dimensions(
+                expanded_width
+            )
 
-        return current_width, current_height
-
-    def __get_dimensions(self, width):
-        time_width, time_height = self.time_paragraph.wrap(width, 0)
-        title_width, title_height = self.title_paragraph.wrap(width, 0)
-        description_width, description_height = self.description_paragraph.wrap(width, 0)
-        max_width = max(max(time_width, title_width), description_width)
-        total_height = time_height + title_height + description_height
-
-        return max_width, total_height
-
-    def draw(self):
-        self.canvas.rect(self.x, self.y, self.width, self.height)
-        time_y = self.y + self.height - self.time_paragraph.height
-        self.time_paragraph.drawOn(
-            self.canvas,
-            self.x,
-            time_y,
-        )
-        title_y = time_y - self.title_paragraph.height
-        self.title_paragraph.drawOn(
-            self.canvas,
-            self.x,
-            title_y
-        )
-        description_y = title_y - self.description_paragraph.height
-        self.description_paragraph.drawOn(
-            self.canvas,
-            self.x,
-            description_y
-        )
+        return current_width + (2 * self.border_size), current_height
