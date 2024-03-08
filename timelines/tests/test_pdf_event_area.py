babaec7c8655eb8004c95421699278fc013b39d2
l7g9 <lukewgregory@gmail.com>
2024-03-08 22:43:19 +0000

    test(PDFEventArea): add units tests for portrait event area

--- timelines/tests/test_pdf_event_area.py
+++ timelines/tests/test_pdf_event_area.py
@@ -22,6 +22,20 @@ class PDFEventAreaTest(TestCase):
             PDFEventEmpty(70, 0, 30, 60)
         )
 
+        cls.pdf_event_area_portrait = PDFEventArea(
+            0,
+            0,
+            80,
+            160,
+            None
+        )
+        cls.pdf_event_area_portrait.events.append(
+            PDFEventEmpty(0, 40, 20, 20)
+        )
+        cls.pdf_event_area_portrait.events.append(
+            PDFEventEmpty(0, 70, 60, 30)
+        )
+
     def test_landscape_position_event(self):
         test_area = Area(10, 0, 20, 30)
         position = self.pdf_event_area_landscape.get_landscape_position(
@@ -90,6 +104,82 @@ class PDFEventAreaTest(TestCase):
     def test_landscape_too_big(self):
         test_area = Area(0, 0, 30, 90)
         position = self.pdf_event_area_landscape.get_landscape_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNone(position)
+
+    def test_portrait_position(self):
+        test_area = Area(0, 10, 20, 20)
+        position = self.pdf_event_area_portrait.get_portrait_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNotNone(position)
+        self.assertEqual(position[0], 0)
+        self.assertEqual(position[1], 10)
+
+    def test_portrait_position_right_of_event(self):
+        test_area = Area(0, 40, 20, 20)
+        position = self.pdf_event_area_portrait.get_portrait_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNotNone(position)
+        self.assertEqual(position[0], 25)
+        self.assertEqual(position[1], 40)
+
+    def test_portrait_position_below_and_right_of_event(self):
+        test_area = Area(0, 60, 30, 30)
+        position = self.pdf_event_area_portrait.get_portrait_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNotNone(position)
+        self.assertEqual(position[0], 25)
+        self.assertEqual(position[1], 35)
+
+    def test_portrait_position_above_event(self):
+        test_area = Area(0, 80, 30, 30)
+        position = self.pdf_event_area_portrait.get_portrait_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNotNone(position)
+        self.assertEqual(position[0], 0)
+        self.assertEqual(position[1], 105)
+
+    def test_portrait_position_off_bottom_edge(self):
+        test_area = Area(0, -20, 30, 30)
+        position = self.pdf_event_area_portrait.get_portrait_position(
+            test_area,
+            True,
+            True,
+            None,
+            5
+        )
+        self.assertIsNotNone(position)
+        self.assertEqual(position[0], 0)
+        self.assertEqual(position[1], -20)
+
+    def test_portrait_too_big(self):
+        test_area = Area(0, 0, 90, 30)
+        position = self.pdf_event_area_portrait.get_portrait_position(
             test_area,
             True,
             True,
