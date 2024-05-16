d916af29dbacdd422ce8b9c5b60ad964db32ef96
l7g9 <lukewgregory@gmail.com>
2024-05-16 23:51:49 +0100

    fix: remove paging from timeline detail views
    no longer required because tmeplates are now using table to handle large numbers of events

--- date_time_timelines/views.py
+++ date_time_timelines/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
 from django.http import FileResponse, HttpResponseForbidden
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -38,17 +37,7 @@ class DateTimeTimelineDetailView(
         context = super().get_context_data(**kwargs)
         context["now"] = timezone.now()
 
-        events = self.get_related_events()
-        context['related_events'] = events
-        context['page_obj'] = events
         return context
-
-    def get_related_events(self):
-        queryset = self.object.datetimeevent_set.all()
-        paginator = Paginator(queryset, 10)
-        page = self.request.GET.get('page')
-        events = paginator.get_page(page)
-        return events
 
 
 class DateTimeTimelineCreateView(LoginRequiredMixin, CreateView):
