41efcf3ba1b51fb6a4b31365776f0d307664d42b
l7g9 <lukewgregory@gmail.com>
2024-08-25 21:14:48 +0100

    fix(ScientificTimeline): improve range of scale units

--- scientific_timelines/models.py
+++ scientific_timelines/models.py
@@ -10,13 +10,19 @@ class ScientificTimeline(timelines.Timeline):
     SCALE_UNITS = [
         (100, "0.1 Thousand Years"),
         (500, "0.5 Thousand Years"),
-        (1000, "1.0 Thousand Years"),
+        (1000, "1 Thousand Years"),
+        (5000, "5 Thousand Years"),
+        (10000, "10 Thousand Years"),
+        (50000, "50 Thousand Years"),
         (100000, "0.1 Million Years"),
         (500000, "0.5 Million Years"),
-        (1000000, "1.0 Million Years"),
+        (1000000, "1 Million Years"),
+        (5000000, "5 Million Years"),
+        (10000000, "10 Million Years"),
+        (50000000, "50 Million Years"),
         (100000000, "0.1 Billion Years"),
         (500000000, "0.5 Billion Years"),
-        (1000000000, "1.0 Billion Years"),
+        (1000000000, "1 Billion Years"),
     ]
     scale_unit = models.IntegerField(choices=SCALE_UNITS, default=1000)
 
